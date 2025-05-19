10a0ed84d3b7e12f7001483f6f6dfe2233e9e79c
Udara <usudarasubodhitha@gmail.com>
2025-05-19 13:41:06 +0530

    Initial commit

---
+++ src/test/java/com/project/rolebasedauthentication/RoleBasedAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rolebasedauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
