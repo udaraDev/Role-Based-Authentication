974fd415a270e3857a0850911952f33d04db70d7
Udara <usudarasubodhitha@gmail.com>
2025-05-29 09:30:14 +0530

    Created restricted endpoints to retrieve users

---
+++ src/main/java/com/project/rolebasedauthentication/services/UserService.java
@@ -0,0 +1,24 @@
+package com.project.rolebasedauthentication.services;
+
+import com.project.rolebasedauthentication.entity.User;
+import com.project.rolebasedauthentication.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers(){
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+
+    }
+}
